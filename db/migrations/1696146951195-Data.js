9776adcfe63b879f0b137c2fadf684ac057c6ac2
ALFONOVA <85473027+Alfonova-Node@users.noreply.github.com>
2023-10-01 19:45:44 +0800

    Update and rename 1696116366134-Data.js to 1696146951195-Data.js

--- db/migrations/1696146951195-Data.js
+++ db/migrations/1696146951195-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1696116366134 {
-    name = 'Data1696116366134'
+module.exports = class Data1696146951195 {
+    name = 'Data1696146951195'
 
     async up(db) {
         await db.query(`CREATE TABLE "gravatar" ("id" character varying NOT NULL, "owner" text NOT NULL, "display_name" text NOT NULL, "image_url" text NOT NULL, CONSTRAINT "PK_e887b4dffafd686933930ef25bb" PRIMARY KEY ("id"))`)
